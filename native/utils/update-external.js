881704cdcce403584dd085a5598746e037e5a7bc
yhh <359807859@qq.com>
2026-01-06 10:08:35 +0800

    build(native): switch external repo to esengine fork
    - Update external-config.json to use esengine/cocos-engine-external
    - Fix update-external.js to read config from external-config.json
    - Change from SSH to HTTPS for easier cloning without SSH keys
    - Update package-lock.json

--- native/utils/update-external.js
+++ native/utils/update-external.js
@@ -5,6 +5,6 @@ const { join } = require('path');
 const git = require('./git');
 
 const externalJSON = JSON.parse(readFileSync(join(__dirname, '../external-config.json')));
-const tag = externalJSON.version;
+const { owner, name, checkout } = externalJSON.from;
 
-git.updateExternal('git@github.com:cocos-creator/cocos2d-x-lite-external.git', tag);
+git.updateExternal(`https://github.com/${owner}/${name}.git`, checkout);
